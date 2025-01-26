2088082a908a123990558fc38170c4fa25c02ae5
mr9 asif <asifalibd002@gmail.com>
2025-01-27 01:24:10 +0600

    added http module

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const os = require('os');
-// console.log(os.cpus().length);
-const fs = require('fs');
-const res = fs.writeFileSync('context.txt');
-console.log(res)
+// const os = require('os');
+// // console.log(os.cpus().length);
+// const fs = require('fs');
+// const res = fs.writeFileSync('context.txt');
+// console.log(res)
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200; // Set HTTP status code
+    res.setHeader('Content-Type', 'text/plain'); // Set headers
+    res.end('Hello, World!'); // Send response body
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000/');
+});
